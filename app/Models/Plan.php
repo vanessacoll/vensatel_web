1eda3893b3e60af80f0a694930fd07c996aebebd
vanessacoll <collvanessa041@gmail.com>
2023-06-20 22:53:09 -0400

    ult cambios form deuda

---
+++ app/Models/Plan.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'planes';
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'id_plan';
+
+     protected $fillable = [
+        'id_plan',
+        'descripcion', 
+        'precio',
+        
+    ];
+
+}
