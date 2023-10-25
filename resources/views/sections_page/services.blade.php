8f3ed113c018f6e7e37a6a0bd6e96bc3fe34406b
Angel-Andarte <angelandarte15@gmail.com>
2023-10-25 18:53:41 -0400

    mapa

--- resources/views/sections_page/services.blade.php
+++ resources/views/sections_page/services.blade.php
@@ -1,140 +1,157 @@
 <section id="services_page" class="bg-light text-center">
     <div class="container">
-      <div class="row">
-        <div class="col">
-          <h1 class="fs-2 fs-sm-4 fs-md-5">Internet Residencial</h1>
-          <p class="lead">Disponemos de Planes desde 4MB. Elige el plan de internet que se ajuste a tus necesidades, consulta el precio de instalación y requisitos con nuestros agentes de ventas. Aseguramos un rendimiento mínimo del 99% o más en nuestra señal, una alta velocidad en conexión y rápida atención ante posibles fallas.</p>
-        </div>
-      </div>
-      <div class="row mt-6">
-        <div class="col-lg-4">
-          <div class="card card-span h-100">
-            <div class="card-span-img"><span class="fas fa-cloud-download-alt fs-4 text-warning"></span></div>
-            <div class="card-body pt-6 pb-4">
-              <h5 class="mb-2">Velocidad de descarga</h5>
-              <p>Gracias a nuestra tecnología inhalambrica se garantiza la velocidad de conexión contratada, debido a que ofrecemos un internet simétrico, que se comprueba su velocidad al momento de la instalación y frente a nuestros clientes.</p>
+        <div class="row">
+            <div class="col">
+                <h1 class="fs-2 fs-sm-4 fs-md-5">Internet Residencial</h1>
+                <p class="lead">Disponemos de Planes desde 4MB. Elige el plan de internet que se ajuste a tus
+                    necesidades, consulta el precio de instalación y requisitos con nuestros agentes de ventas.
+                    Aseguramos un rendimiento mínimo del 99% o más en nuestra señal, una alta velocidad en conexión y
+                    rápida atención ante posibles fallas.</p>
             </div>
-          </div>
         </div>
-        <div class="col-lg-4 mt-6 mt-lg-0">
-          <div class="card card-span h-100">
-            <div class="card-span-img"><span class="fas fa-hdd fs-5 text-warning"></span></div>
-            <div class="card-body pt-6 pb-4">
-              <h5 class="mb-2">Equipos de última generación</h5>
-              <p>Contamos con equipos de avanzada tecnología que permiten que nuestros clientes tengan la calidad de servicios que se merecen, adicional ofrecemos un internet de fibra óptica con equipos Wi-fi.</p>
+        <div class="row mt-6">
+            <div class="col-lg-4">
+                <div class="card card-span h-100">
+                    <div class="card-span-img"><span class="fas fa-cloud-download-alt fs-4 text-warning"></span></div>
+                    <div class="card-body pt-6 pb-4">
+                        <h5 class="mb-2">Velocidad de descarga</h5>
+                        <p>Gracias a nuestra tecnología inhalambrica se garantiza la velocidad de conexión contratada,
+                            debido a que ofrecemos un internet simétrico, que se comprueba su velocidad al momento de la
+                            instalación y frente a nuestros clientes.</p>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mt-6 mt-lg-0">
-          <div class="card card-span h-100">
-            <div class="card-span-img"><span class="fas fa-wifi fs-6 text-warning"></span></div>
-            <div class="card-body pt-6 pb-4">
-              <h5 class="mb-2">Internet Rápido e ilimitado</h5>
-              <p>Ofrecemos un servicio de Internet insuperable, ya que además de su alta velocidad de descarga, también es un servicio de internet completamente ilimitado, lo que te permite descargar todo lo que quieras.</p>
+            <div class="col-lg-4 mt-6 mt-lg-0">
+                <div class="card card-span h-100">
+                    <div class="card-span-img"><span class="fas fa-hdd fs-5 text-warning"></span></div>
+                    <div class="card-body pt-6 pb-4">
+                        <h5 class="mb-2">Equipos de última generación</h5>
+                        <p>Contamos con equipos de avanzada tecnología que permiten que nuestros clientes tengan la
+                            calidad de servicios que se merecen, adicional ofrecemos un internet de fibra óptica con
+                            equipos Wi-fi.</p>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div class="col-lg-4 mt-6 mt-lg-0">
+                <div class="card card-span h-100">
+                    <div class="card-span-img"><span class="fas fa-wifi fs-6 text-warning"></span></div>
+                    <div class="card-body pt-6 pb-4">
+                        <h5 class="mb-2">Internet Rápido e ilimitado</h5>
+                        <p>Ofrecemos un servicio de Internet insuperable, ya que además de su alta velocidad de
+                            descarga, también es un servicio de internet completamente ilimitado, lo que te permite
+                            descargar todo lo que quieras.</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
+</section>
 
 
 <!------------------------------------------------ prices section --------------------------------------------->
 
 <section class="">
-    <div class="bg-holder " style="background-image:url(page_template/img/mapas.png);background-position: center top;"></div>
+    <div class="bg-holder " style="background-image:url(page_template/img/mapas.png);background-position: center top;">
+    </div>
     <!--/.bg-holder-->
     <div class="container">
 
-      <div class="row">
-        <div class="col text-center">
-          <h1 class="fs-2 fs-sm-4 fs-md-5 text-primary">Nuestros Planes</h1>
-          <p class="lead text-primary"><strong>La mejor velocidad para tu hogar.</strong></p>
-        </div>
-      </div>
-
-      <div class="card mt-3">
-            <div class="card-body">
-              <div class="row g-0">
-                <div class="col-12 mb-3">
-                  <div class="row justify-content-center justify-content-sm-between">
-                    <div class="col-sm-auto text-center">
-                      <h5 class="d-inline-block">Tarifas Mensuales</h5>
-                    </div>
-
-                  </div>
-                </div>
-                <div class="col-lg-4 border-top border-bottom">
-                  <div class="h-100">
-                    <div class="text-center p-4">
-                      <h3 class="fw-normal my-0">Residencial</h3>
-                      <p class="mt-3">4 MBPS.</p>
-                      <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>35<small class="fs--1 text-700">/ mes</small></h2>
-
-                      <a class="btn btn-outline-primary" href="{{ route('index') }}">Solicitar Servicio</a>
-                    </div>
-
-                    <hr class="border-bottom-0 m-0" />
-
-                    <div class="text-center p-4">
-                      <h3 class="fw-normal my-0">Residencial</h3>
-                      <p class="mt-3">10 MBPS.</p>
-                      <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>90<small class="fs--1 text-700">/ mes</small></h2>
-
-                      <a class="btn btn-outline-primary" href="{{ route('index') }}">Solicitar Servicio</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-4 border-top border-bottom dark__bg-1000 px-4 px-lg-0" style="background-color: rgba(115, 255, 236, 0.18);">
-                  <div class="h-100">
-                    <div class="text-center p-4">
-                      <h3 class="fw-normal my-0">Residencial</h3>
-                      <p class="mt-3">6 MBPS.</p>
-                      <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>60<small class="fs--1 text-700">/ mes</small></h2>
-
-                      <a class="btn btn-primary" href="{{ route('index') }}">Solicitar Servicio</a>
-                    </div>
-                     <hr class="border-bottom-0 m-0" />
-
-                      <div class="text-center p-4">
-                      <h3 class="fw-normal my-0">Residencial</h3>
-                      <p class="mt-3">16 MBPS.</p>
-                      <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>150<small class="fs--1 text-700">/ mes</small></h2>
-
-                      <a class="btn btn-primary" href="{{ route('index') }}">Solicitar Servicio</a>
-                    </div>
-
-                  </div>
-                </div>
-                <div class="col-lg-4 border-top border-bottom">
-                  <div class="h-100">
-                    <div class="text-center p-4">
-                      <h3 class="fw-normal my-0">Residencial</h3>
-                      <p class="mt-3">8 MBPS.</p>
-                      <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>75<small class="fs--1 text-700">/ mes</small></h2>
-
-                      <a class="btn btn-outline-primary" href="{{ route('index') }}">Solicitar Servicio</a>
-                    </div>
-
-                    <hr class="border-bottom-0 m-0" />
-
-                    <div class="text-center p-4">
-                      <h3 class="fw-normal my-0">Residencial</h3>
-                      <p class="mt-3">8 MBPS.</p>
-                      <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>75<small class="fs--1 text-700">/ mes</small></h2>
-
-                      <a class="btn btn-outline-primary" href="{{ route('index') }}">Solicitar Servicio</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-12 text-center">
-                  <h5 class="mt-5"></h5>
-                </div>
-              </div>
+        <div class="row">
+            <div class="col text-center">
+                <h1 class="fs-2 fs-sm-4 fs-md-5 text-primary">Nuestros Planes</h1>
+                <p class="lead text-primary"><strong>La mejor velocidad para tu hogar.</strong></p>
             </div>
-          </div>
+        </div>
+
+        <div class="card mt-3">
+            <div class="card-body">
+                <div class="row g-0">
+                    <div class="col-12 mb-3">
+                        <div class="row justify-content-center justify-content-sm-between">
+                            <div class="col-sm-auto text-center">
+                                <h5 class="d-inline-block">Tarifas Mensuales</h5>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-4 border-top border-bottom">
+                        <div class="h-100">
+                            <div class="text-center p-4">
+                                <h3 class="fw-normal my-0">Residencial</h3>
+                                <p class="mt-3">4 MBPS.</p>
+                                <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>35<small
+                                        class="fs--1 text-700">/ mes</small></h2>
+
+                                <a class="btn btn-outline-primary" href="{{ route('index') }}">Solicitar Servicio</a>
+                            </div>
+
+                            <hr class="border-bottom-0 m-0" />
+
+                            <div class="text-center p-4">
+                                <h3 class="fw-normal my-0">Residencial</h3>
+                                <p class="mt-3">10 MBPS.</p>
+                                <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>90<small
+                                        class="fs--1 text-700">/ mes</small></h2>
+
+                                <a class="btn btn-outline-primary" href="{{ route('index') }}">Solicitar Servicio</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 border-top border-bottom dark__bg-1000 px-4 px-lg-0"
+                        style="background-color: rgba(115, 255, 236, 0.18);">
+                        <div class="h-100">
+                            <div class="text-center p-4">
+                                <h3 class="fw-normal my-0">Residencial</h3>
+                                <p class="mt-3">6 MBPS.</p>
+                                <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>60<small
+                                        class="fs--1 text-700">/ mes</small></h2>
+
+                                <a class="btn btn-primary" href="{{ route('index') }}">Solicitar Servicio</a>
+                            </div>
+                            <hr class="border-bottom-0 m-0" />
+
+                            <div class="text-center p-4">
+                                <h3 class="fw-normal my-0">Residencial</h3>
+                                <p class="mt-3">16 MBPS.</p>
+                                <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>150<small
+                                        class="fs--1 text-700">/ mes</small></h2>
+
+                                <a class="btn btn-primary" href="{{ route('index') }}">Solicitar Servicio</a>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-4 border-top border-bottom">
+                        <div class="h-100">
+                            <div class="text-center p-4">
+                                <h3 class="fw-normal my-0">Residencial</h3>
+                                <p class="mt-3">8 MBPS.</p>
+                                <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>75<small
+                                        class="fs--1 text-700">/ mes</small></h2>
+
+                                <a class="btn btn-outline-primary" href="{{ route('index') }}">Solicitar Servicio</a>
+                            </div>
+
+                            <hr class="border-bottom-0 m-0" />
+
+                            <div class="text-center p-4">
+                                <h3 class="fw-normal my-0">Residencial</h3>
+                                <p class="mt-3">8 MBPS.</p>
+                                <h2 class="fw-medium my-4"> <sup class="fw-normal fs-2 me-1">&dollar;</sup>75<small
+                                        class="fs--1 text-700">/ mes</small></h2>
+
+                                <a class="btn btn-outline-primary" href="{{ route('index') }}">Solicitar Servicio</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 text-center">
+                        <h5 class="mt-5"></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
-      </div>
+    </div>
     </div><!-- end of .container-->
-  </section>
+</section>
